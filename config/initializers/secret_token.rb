f6022e8a3d6548d3632e9e34f3a7af6d3cd2a1e7
Jeremy PELE <jpele@mint>
2014-02-10 14:10:33 +0100

    Create skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'd5705c6afb44669f0dec4989118949b0c678cd50808b0e27d98be9e6b6b94e28779c912cf5c271fc6182be83f72ddd6bdcdc318b49459b9afe25ce3713dffabd'
