9d85f0368cef9aff9c961b16f2b19ad5faee03a0
Jeremy PELE <jpele@mint>
2014-02-10 14:42:06 +0100

    Update back-office product generator form

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,38 @@
 ActiveAdmin.register Product do
 
-  
+	form do |f|
+ 		f.semantic_errors *f.object.errors.keys
+ 		
+      	f.inputs "Produit" do # Product's fields
+          	f.input :title, :label => 'Titre'
+          	f.input :desc , :label => 'Description'      
+
+
+          	f.input :price, :label => 'Prix'
+          	f.input :price_unit, :label => 'Price unit'
+          	# , :as => :select, :collection => [['€', 'euros'], ['$', 'dollars']]
+    
+
+          	f.input :weight
+          	f.input :weight_unit, :label => 'Weight unit'
+          	# , :as => :select, :collection => [['kgs', 'kilos'], ['pds', 'pounds']]
+    
+
+          	f.input :is_published, :label => 'Visible', :as => :boolean 
+          	f.input :position, :label => 'Position'
+          	   
+      	end
+    
+        f.has_many :product_categories, :allow_destroy => true, :heading => 'Categories' do |cf|
+          cf.input :category
+        end
+
+        f.has_many :product_pictures, :allow_destroy => true, :heading => 'Pictures',:new_record => true do |cf|
+           cf.input :url
+           cf.input :legend
+        end
+
+      	f.actions
+  	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
   attr_accessible :desc, :is_published, :position, :price, :price_unit, :title, :weight, :weight_unit
+
+  validates :title, presence: true, length: { minimum: 1 }
+
+  attr_accessible :product_categories_attributes
+  has_many :product_categories 
+  accepts_nested_attributes_for :product_categories, :allow_destroy => true
+
+  attr_accessible :product_pictures_attributes
+  has_many :product_pictures 
+  accepts_nested_attributes_for :product_pictures, :allow_destroy => true
 end
